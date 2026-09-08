9bb35950afdc85a82c515a84268ce40d2d011194
Damy Gaso <122506808+DAMY-GASO@users.noreply.github.com>
2026-09-08 06:34:30 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LanguageProvider } from "./context/LanguageContext.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import BottomNav from "./components/BottomNav.jsx";
 import Navbar from "./components/Navbar.jsx";
@@ -8,19 +7,17 @@ import "./index.css";
 
 function App() {
   return (
-    <LanguageProvider>
-      <Router>
-        <div className="app-wrapper">
-          <Navbar />
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<LandingPage />} />
-            </Routes>
-          </main>
-          <BottomNav />
-        </div>
-      </Router>
-    </LanguageProvider>
+    <Router>
+      <div className="app-wrapper">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        </main>
+        <BottomNav />
+      </div>
+    </Router>
   );
 }
 
