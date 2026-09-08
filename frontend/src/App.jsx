9226adee1573e51a60d47324c78e6026abd4ef94
Damy Gaso <122506808+DAMY-GASO@users.noreply.github.com>
2026-09-08 06:17:08 -0700

    Wrap App component in LanguageProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { LanguageProvider } from "./context/LanguageContext.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import BottomNav from "./components/BottomNav.jsx";
 import Navbar from "./components/Navbar.jsx";
@@ -7,17 +8,19 @@ import "./index.css";
 
 function App() {
   return (
-    <Router>
-      <div className="app-wrapper">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-          </Routes>
-        </main>
-        <BottomNav />
-      </div>
-    </Router>
+    <LanguageProvider>
+      <Router>
+        <div className="app-wrapper">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+            </Routes>
+          </main>
+          <BottomNav />
+        </div>
+      </Router>
+    </LanguageProvider>
   );
 }
 
