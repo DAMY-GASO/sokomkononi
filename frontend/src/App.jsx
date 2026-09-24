7b6c9504de37983c84de0ed2e1a85ff84fb5ebe0
Damy Gaso <122506808+DAMY-GASO@users.noreply.github.com>
2026-09-24 20:05:05 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ import ContactPage from "./pages/ContactPage.jsx";
 import TermsPage from "./pages/TermsPage.jsx";
 import PrivacyPage from "./pages/PrivacyPage.jsx";
 import JinsiYaKununuaNaKuuza from "./pages/JinsiYaKununuaNaKuuza.jsx";
-import InviteFriends from "./pages/InviteFriends.jsx";
+import InviteFriendsPage from "./pages/InviteFriendsPage.jsx";
 
 // ============================================================
 // AUTH PAGES
